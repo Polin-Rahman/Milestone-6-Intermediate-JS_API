330156a63a884079d9a0d08cc753bb129c98f52c
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2021-08-23 19:53:13 +0600

    template-string

---
+++ module-30/template-string/template-string.js
@@ -0,0 +1,19 @@
+// single quotatio
+const mon = 'its monday';
+
+// dauble quotatio
+const sun = "its sunday";
+
+// back quote (multiple line possible)
+const fri = `its friday.
+its holyday!`;
+
+//its works as dynamic string
+const first = 'Polin';
+const last = 'Rahman';
+
+const old = first + ' ' + last;
+const New = `${first} ${last}`;
+
+console.log(old);
+console.log(New);
